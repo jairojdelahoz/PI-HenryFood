00a4487b01fef6afc789d407b060d0d2d983c0a8
jairojdelahoz <jairojdelahozv@gmail.com>
2023-07-17 15:32:11 -0500

    Prueba Commit

--- api/src/db.js
+++ api/src/db.js
@@ -33,7 +33,7 @@ sequelize.models = Object.fromEntries(capsEntries);
 const { Recipe } = sequelize.models;
 
 // Aca vendrian las relaciones
-// Product.hasMany(Reviews);
+// Product.hasMany(Reviews); Prueba Commit
 
 module.exports = {
   ...sequelize.models, // para poder importar los modelos así: const { Product, User } = require('./db.js');
